4accd18b02c3aca4439b88b0866fc4eedea836ec
yi.mei <yi.mei@zhenai.com>
2019-06-14 19:07:59 +0800

    提交注册信息

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/mikemei5/dist/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
